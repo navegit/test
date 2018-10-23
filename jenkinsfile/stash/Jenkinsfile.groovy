45300a7887580270a866670f38ab59d71005917a
Vangeti, Naveen (Contingent Worker) <NRVangeti@comerica.com>
2018-10-23 17:51:19 -0400

    push

--- jenkinsfile/stash/Jenkinsfile.groovy
+++ jenkinsfile/stash/Jenkinsfile.groovy
@@ -7,5 +7,27 @@ pipeline {
                 cleanWs()
             }
         }
+        stage('Stash') {
+            steps {
+                sh '''
+                    mkdir -p output
+                    cd output
+                    touch myoutput
+                    mkdir -p test1
+                '''
+
+                // Write a text file there.
+                writeFile file: "output/test1/somefile", text: "Hey look, some text."
+
+                // Stash that directory and file.
+                // Note that the includes could be "output/", "output/*" as below, or even
+                // "output/**/*" - it all works out basically the same.
+                // dir('my-dir'){
+                //    git branch: 'master', url: 'https://github.com/navegit/testproj.git'
+                // }
+                // stash name: "first-stash", includes: "my-dir/*"
+                stash name: 'first-stash', includes: 'output/**/*'
+            }
+        }
     }
 }
